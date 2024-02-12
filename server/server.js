c2006e5eaf3a26b2310e588ed117cc47c9001bae
DidIrb <irbayedubadida@gmail.com>
2024-02-12 23:37:15 +0300

    initialized client using vite

--- server/server.js
+++ server/server.js
@@ -2,15 +2,14 @@ import express from 'express';
 import connectDB from "./config/dbConn.js"
 
 const app = express();
-const port = 3000;
-
+const PORT = process.env.PORT || 3500;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Express!');
+    res.send('Hello from Express Service!');
 });
 
 connectDB();
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
